5a48ceca5bce40d3c4fd25206a2d3c6796e2d457
DoolyDoori <leedoori11011@gmail.com>
2023-05-19 01:45:02 +0900

    [ADD] MovieApp 과제완료

--- LeeDooRi/ReactJS/react-for-beginners/src/App.js
+++ LeeDooRi/ReactJS/react-for-beginners/src/App.js
@@ -1,32 +1,37 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
+// App 컴포넌트 정의
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      {/* React Router를 사용하여 경로에 따라 다른 컴포넌트를 렌더링하는 Switch */}
+      <Switch>
+        {/* "/about-us" 경로에 대한 라우트 */}
+        <Route path="/about-us">
+          {/* "Hello"라는 텍스트를 표시하는 h1 요소 */}
+          <h1>Hello</h1>
+        </Route>
+        {/* "/movie/:id" 경로에 대한 라우트 */}
+        <Route path="/movie/:id">
+          {/* Detail 컴포넌트를 렌더링 */}
+          <Detail />
+        </Route>
+        {/* 기본 경로 ("/")에 대한 라우트 */}
+        <Route path="/">
+          {/* Home 컴포넌트를 렌더링 */}
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+/**
+ * App.js는 이제 영화에 대해서 보여주는것이 아닌 router를 render한다.
+ *React Router는 동적 URL을 지원한다.-> URL에 변수를 넣을 수 있다 (EX :Movie/121212) ,
+ * - 위 코드에서 /movie/:id 를 통해 각 영화의 id에 해당하는 정보에 접근 할 수 있게 한다.
+ */
