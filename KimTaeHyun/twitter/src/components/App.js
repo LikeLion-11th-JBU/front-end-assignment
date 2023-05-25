2c85eedfe8479dcf95cf3c120d3ea84377abddb0
tae <kth4111502@naver.com>
2023-05-25 09:31:34 +0900

    [ADD]Firebase버전 변경, Creating Account

--- KimTaeHyun/twitter/src/components/App.js
+++ KimTaeHyun/twitter/src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
-      <footer>&copy; {new Date().getFullYear()} Twitter</footer>
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }
