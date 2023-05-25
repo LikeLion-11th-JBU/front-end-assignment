ba50403b97f52c8fd67aac9e1a0d8d594aa17f93
tae <kth4111502@naver.com>
2023-05-25 16:49:11 +0900

    [ADD]Image를 Firebase Storage에 Upload

--- KimTaeHyun/twitter/src/routes/Home.js
+++ KimTaeHyun/twitter/src/routes/Home.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
+
 import Tweet from "components/Tweet";
 
 const Home = ({ userObj }) => {
@@ -17,12 +19,15 @@ const Home = ({ userObj }) => {
   }, []);
   const onSubmit = async(event) => {
     event.preventDefault();
-    await dbService.collection("tweets").add({
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    console.log(response);
+    /*await dbService.collection("tweets").add({
         text: tweet,
         createdAt: Date.now(),
         creatorId: userObj.uid,
       });
-      setTweet("");
+      setTweet("");*/
     };
   const onChange = (event) => {
     const {
