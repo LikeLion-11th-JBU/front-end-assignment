27a885e466960daa2b359d458d8256bc1a2113b1
kyungbin <pkb8839@naver.com>
2023-05-25 13:43:46 +0900

    [ADD] Logout 버튼 구현

--- ParkKyungBin/nwitter/nwitter/src/components/Router.js
+++ ParkKyungBin/nwitter/nwitter/src/components/Router.js
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
+                    <>
                     <Route path="/" element={<Home />}>
                     </Route>
+                    <Route path="/profile" element={<Profile />}>
+                    </Route>
+                    </>
                 ) : (
                     <Route path="/" element={<Auth />}>
                     </Route>
+        
                     )}   
             </Routes>
         </Router>
