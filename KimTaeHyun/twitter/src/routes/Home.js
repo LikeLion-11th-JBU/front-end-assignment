51db4d81315b6a944ea04e71c0b63130f3b1d1d8
tae <kth4111502@naver.com>
2023-05-25 16:18:54 +0900

    [ADD]Delete and Update

--- KimTaeHyun/twitter/src/routes/Home.js
+++ KimTaeHyun/twitter/src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
+import Tweet from "components/Tweet";
 
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
@@ -43,9 +44,11 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {tweets.map((tweet) => (
-          <div key={tweet.id}>
-            <h4>{tweet.text}</h4>
-          </div>
+           <Tweet
+           key={tweet.id}
+           tweetObj={tweet}
+           isOwner={tweet.creatorId === userObj.uid}
+         />
         ))}
       </div>
     </div>
